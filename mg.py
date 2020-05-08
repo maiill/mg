d7b429856cc0e0c4b1adf4caf73a4b7cc6efd115
maiill <65006563+maiill@users.noreply.github.com>
2020-05-08 06:24:23 +0000

    Add files via upload

---
+++ mg.py
@@ -0,0 +1,61 @@
+#!/bin/usr/python
+
+import random
+import sys
+import time
+def mg(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+#kecepatan mg
+        time.sleep(random.random() * 0.1)
+mg('\33[33;1m<<<<<<<<<<<+>>>>>>>>>>>')
+mg('\33[31;1m      M A I I L L')
+mg('\33[37;1m      G A M I N G')
+mg('\33[33;1m<<<<<<<<<<<+>>>>>>>>>>>\n')
+
+mg('\33[34;1mkalian semua anak hasil\nkondom bocor\n')
+
+import random
+import sys
+import time
+def m(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+#kecepatan m
+        time.sleep(random.random() * 0.4)
+
+m('\33[31;1mHarap diperhatikan!\nmuka anda sudah jelek\nsegeralah operasi plastik\nseperti BTS\nawokawok:v\n')
+print "\33[37;1m#===================================#"
+print "\33[35;1m    T U  N G G U  S E B E N T A R"
+print "\33[33;1m    L O G I N - D U L U - B O S S"
+print "\33[37;1m#===================================#"
+user = raw_input("\33[34;1mUsername : ")
+import getpass
+sandi = getpass.getpass()
+if sandi == 'maiill' and user == 'gaming':
+     print "\33[32;1mLogin Succes"
+else:
+     print "\33[31;1mLogin Failed Please Check Password"
+
+import random
+import sys
+import time
+def g(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+#kecepatan g
+        time.sleep(random.random() * 0.10)
+g('\33[31;1m 10%\n')
+g('\33[33;1m  20%\n')
+g('\33[32;1m   30%\n')
+g('\33[36;1m    40%\n')
+g('\33[34;1m     50%\n')
+g('\33[35;1m     60%\n')
+g('\33[37;1m    70%\n')
+g('\33[31;1m   80%\n')
+g('\33[33;1m  90%\n')
+g('\33[32;1m100%\n')
+print "\33[31;1mMAAF ANDA TIDAK DAPAT MASUK\nPASTIKAN PERANGKAT ANDA TIDAK DIROOT!"
